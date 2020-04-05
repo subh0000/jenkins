ee3d025bd2964beb6e619c2efa11e4a24492b72c
subh <subhasish000@gmail.com>
2020-04-05 19:16:12 +1000

    added java files

---
+++ java.bat
@@ -0,0 +1,7 @@
+set JAVA_HOME="D:\Jenkins\Java\jdk1.8.0_231\bin"
+cd C:\Users\Subhasish\Desktop\JavaProject\
+"C:\Program Files\Java\jdk1.8.0_231\bin\javac" "C:\Users\Subhasish\Desktop\JavaProject\hello.java"
+echo "\n Compilation Complete"
+cd C:\Users\Subhasish\Desktop\JavaProject\
+java -cp C:\Users\Subhasish\Desktop\JavaProject\ hello
+echo "Execution Complete"
