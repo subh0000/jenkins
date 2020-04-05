897d31ec58233ac46eab3a6fbf0ef36543b606a0
subh <subhasish000@gmail.com>
2020-04-05 20:48:43 +1000

    added test files

--- hello.java
+++ hello.java
@@ -1,9 +1,10 @@
 public class hello{
     public static void main(String[] args){
    
-       for(int i=1;i<=35;i++){
+       for(int i=1;i<=15;i++){
 
                 System.out.println("Hello World...."+i);
+				System.out.println("Testing Catlight...."+i);
 
 }
 }
